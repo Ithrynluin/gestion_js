b01e8e058328b600d185c6c414ed482ea001a1b6
benitol87 <benoit.leroy@etu.unilim.fr>
2015-02-16 10:38:36 +0100

    Résolution de l erreur Bootstrap needs jQuery

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -3,9 +3,9 @@
 			Gérance 2015
 		</div>
 
+		<script src='js/jquery.min.js'></script>
 		<script src='js/moment.min.js'></script>
 		<script src='js/bootstrap.min.js'></script>
-		<script src='js/jquery.min.js'></script>
 		<script src='js/fullcalendar.min.js'></script>
 		<script src='js/fullcalendar.fr.js'></script>
 	</body>
