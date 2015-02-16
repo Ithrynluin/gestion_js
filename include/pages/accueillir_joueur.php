d575b14a74970950e56378edb9aee8cb918219cf
clem19s <clemj1900@gmail.com>
2015-02-16 10:07:32 +0100

    un peu de php dans accueillir les joueurs

--- include/pages/accueillir_joueur.php
+++ include/pages/accueillir_joueur.php
@@ -1,28 +1,33 @@
 <h1>Accueillir Joueur</h1>
-
-<form method="post" action="">
-	<p>
-		<label for="pseudo">Pseudo : </label>
-		<input type="text" id="pseudo" name="pseudo"/>
-	</p>
-	<p>
-		<label for="zone">Zone : </label>
-		<select id="zone" name="zone">
-		</select>
-	</p>
-	<p>
-		<input type="submit" valeu="Valider">
-	</p>
-</form>
-
-<div id="confirmation">
-	<p>
-		Zone : 
-	</p>
-	<p>
-		Machine :
-	</p>
-	<p>
-		<a href="accueillir_joueur.php">Accueillir un nouveau joueur</a>
-	</p>
-</div>
+<?php
+if(empty($POST["pseudo"]) || empty($POST["zone"])){  ?>
+	<form method="post" action="accueillir_joueur.php">
+		<p>
+			<label for="pseudo">Pseudo : </label>
+			<input type="text" id="pseudo" name="pseudo"/>
+		</p>
+		<p>
+			<label for="zone">Zone : </label>
+			<select id="zone" name="zone">
+			</select>
+		</p>
+		<p>
+			<input type="submit" valeu="Valider">
+		</p>
+	</form>	
+<?php
+}else{ ?>
+	<div id="confirmation">
+		<p>
+			Zone : 
+		</p>
+		<p>
+			Machine :
+		</p>
+		<p>
+			<a href="accueillir_joueur.php">Accueillir un nouveau joueur</a>
+		</p>
+	</div>
+<?php
+}
+?>
