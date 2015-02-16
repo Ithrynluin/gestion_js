a3b4f62a5d09df28fa7119899390f7f4ca0b0199
clem19s <clemj1900@gmail.com>
2015-02-16 10:01:44 +0100

    interface pour accueillir les joueurs

---
+++ include/pages/accueillir_joueur.php
@@ -0,0 +1,28 @@
+<h1>Accueillir Joueur</h1>
+
+<form method="post" action="">
+	<p>
+		<label for="pseudo">Pseudo : </label>
+		<input type="text" id="pseudo" name="pseudo"/>
+	</p>
+	<p>
+		<label for="zone">Zone : </label>
+		<select id="zone" name="zone">
+		</select>
+	</p>
+	<p>
+		<input type="submit" valeu="Valider">
+	</p>
+</form>
+
+<div id="confirmation">
+	<p>
+		Zone : 
+	</p>
+	<p>
+		Machine :
+	</p>
+	<p>
+		<a href="accueillir_joueur.php">Accueillir un nouveau joueur</a>
+	</p>
+</div>
