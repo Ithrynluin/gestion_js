96142fd40f4b98b2e4e76685f2a038ce06fb7725
Adeline <adeline_mignot@hotmail.fr>
2015-02-18 09:43:43 +0100

    début gestion machines ...

--- include/pages/gererMachines.inc.php
+++ include/pages/gererMachines.inc.php
@@ -1,1 +1,26 @@
 <h1>Machines</h1>
+<h2>Rendre une machine indisponible</h2>
+<link href='css/machine.css' rel='stylesheet' />
+<select id='liste'>
+	<option value='0'>Zone</option>
+	<option value='1'>Gamers</option>
+	<option value='2'>Mélomanes</option>
+	<option value='3'>Barrés</option>
+</select>
+<script src='js/lister_machine.js'></script>
+<!-- <div class='table'>
+	table 1 :
+	<span class='machine'></span>
+	<span class='machine'></span>
+	<span class='machine'></span>
+	<span class='machine'></span>
+	<span class='machine'></span>
+</div>
+<div class='table'>
+	table 2 :
+	<span class='machine'></span>
+	<span class='machine'></span>
+	<span class='machine'></span>
+	<span class='machine'></span>
+	<span class='machine'></span>
+</div> -->
