d5190a975ad992148f62d95ea3bbfb3630dffab2
Camille Blaser <camille.blaser@gmail.com>
2015-03-02 09:39:15 +0100

    Débiter et Créditer

---
+++ include/pages/debiter.php
@@ -0,0 +1,44 @@
+<?php
+if(isset($_POST['login']) && isset($_POST['montant'])){
+	$jsonString = file_get_contents(dirname(__FILE__).'/../../json/compte.json');
+	$data = json_decode($jsonString);
+
+	foreach ($data as $key => $value) {
+		if($value->login == $_POST['login']){
+			$id = $key;
+		}
+	}
+	
+	$credits = $data[$id]->nbCredit;
+	$sup = intval($_POST['montant']);
+	
+	if($credits > $sup){
+		$data[$id]->nbCredit = $credits - $sup;
+		$newJsonString = json_encode($data);
+		file_put_contents(dirname(__FILE__) . '/../../json/compte.json', $newJsonString);
+	}
+	else{
+		echo 'Vous n\'avez pas assez de crédits.';
+	}
+	
+}
+else{
+?>
+<script src='js/modifierCredits.js'></script>
+<title>Crédits</title>
+<div id='credit'>
+	<form action="#" method="POST">
+		<label>Login du compte</label>
+		<input id ="login" name="login" type="text">
+		<br>
+		<p id="credits"> Crédits : <span></span></p>
+		<br>
+		<label>Montant</label>
+		<input name="montant" type="text">
+		<br>
+		<input type="submit" value="Débiter">
+	</form>
+</div>
+<?php
+}
+?>
