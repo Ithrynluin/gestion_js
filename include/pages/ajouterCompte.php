3819899db7d59d437d87d9f21cce37e99ffa9b15
Camille Blaser <camille.blaser@gmail.com>
2015-03-04 19:14:33 +0100

    Correction d'erreurs sur le formulaire d'ajout de compte'

--- include/pages/ajouterCompte.php
+++ include/pages/ajouterCompte.php
@@ -1,29 +1,12 @@
 <h1>Ajouter un compte</h1>
 	<?php
-	if(isset($_POST['login']) && isset($_POST['mail']) && isset($_POST['credit']) && isset($_POST['mdp'])){
-		
-		$jsonString = file_get_contents('./json/compte.json');
-		$data = json_decode($jsonString);
-
-		$rang = sizeof($data);
-		$data[$rang] = [];
-		$data[$rang]['idCompte'] = $rang+1;
-		$data[$rang]['nbCredit'] = intval($_POST['credit']);
-		$data[$rang]['mail'] = $_POST['mail'];
-		$data[$rang]['login'] = $_POST['login'];
-		$data[$rang]['password'] = $_POST['mdp'];
-		$data[$rang]['nom'] = $_POST['nom'];
-		$data[$rang]['prenom'] = $_POST['prenom'];
-
-		$newJsonString = json_encode($data);
-		file_put_contents('./json/compte.json', $newJsonString);
-	}
-	else{
+	if(empty($_POST['login']) || empty($_POST['mail']) || empty($_POST['credit']) || empty($_POST['mdp'])){
 	?>
+		<script src='js/ajout_compte.js'></script>
 		<div id="compte">
 			<form action="#" method="POST">	
 				<label>Nom</label>
-				<input name="nom" type="text">
+				<input id="nom" name="nom" type="text">
 				<br>
 				<label>Prénom</label>
 				<input name="prenom" type="text">
@@ -40,9 +23,32 @@
 				<label>Mot de Passe</label>
 				<input name="mdp" type="password">
 				<br>
-				<input type="submit" value="Ajouter">
+				<input type="submit" id="valider" value="Ajouter">
 			</form>	
 		</div>
 	<?php
+		
+	}
+	else{
+		$jsonString = file_get_contents('./json/compte.json');
+		$data = json_decode($jsonString);
+
+		$rang = sizeof($data);
+		$data[$rang] = [];
+		$data[$rang]['idCompte'] = $rang+1;
+		$data[$rang]['nbCredit'] = intval($_POST['credit']);
+		$data[$rang]['mail'] = $_POST['mail'];
+		$data[$rang]['login'] = $_POST['login'];
+		$data[$rang]['password'] = $_POST['mdp'];
+		$data[$rang]['nom'] = $_POST['nom'];
+		$data[$rang]['prenom'] = $_POST['prenom'];
+
+		$newJsonString = json_encode($data);
+		file_put_contents('./json/compte.json', $newJsonString);
+		echo 'Le compte de ';
+		echo $_POST['prenom'];
+		echo ' ';
+		echo $_POST['nom'];
+		echo ' a bien été ajouté.';
 	}
 	?>
