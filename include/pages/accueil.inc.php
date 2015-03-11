a90059039bcfc24f2e6d25fe0fcc390196beeb38
benitol87 <benoit.leroy@etu.unilim.fr>
2015-03-11 15:44:55 +0100

    Password checking:checked

--- include/pages/accueil.inc.php
+++ include/pages/accueil.inc.php
@@ -1,12 +1,13 @@
 <h1>Accueil</h1>
 
+<script type="text/javascript" src="js/md5.js"></script>
 <script type="text/javascript">
 	$(function(){
-		obj = {idZone:2};
+		obj = {login:"testt", password:md5("lapin")};
 		$.ajax({
 			data:obj,
 			type:'GET',
-			url:'php/getChiffreAffaires.php',
+			url:'php/checkPassword.php',
 			success:function(data){
 				// Affichage si tout se passe bien
 				console.log(data);
@@ -15,5 +16,8 @@
 				console.log("Erreur update");
 			}
 		});
+		//console.log(md5("lapin"));
 	});
+
+
 </script>
