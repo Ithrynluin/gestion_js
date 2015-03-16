69f4b0eb54c8f665e61a7e746c295637b26b9075
Camille Blaser <camille.blaser@gmail.com>
2015-03-16 20:29:01 +0100

    ajout compte

--- js/modifierCredits.js
+++ js/modifierCredits.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+	$("form").on("submit", function(){
+		var login = $("#login").val();
+		var montant = $("#montant").val();
+		console.log(login);
+		console.log(montant);
+	
+		$.ajax({
+				url: 'php/crediter.php',
+				type: 'GET',
+				/*dataType: 'json',*/
+				data: {login: login, montant: montant},
+				success:function(data){
+					console.log("Compte crédité")
+				},
+				error:function(data){
+					console.log("Erreur")
+				}
+			})
+		return false;
+	});
+	
 	$("#login").on("change",function(){
 		login = this.value;
 		$.ajax({
